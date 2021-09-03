3125a9fb6616f92ff95b7910cd202558bf591ffb
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2021-09-03 15:34:10 +0800

    updated the directory name

--- alpha/badge/views.py
+++ alpha/badge/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "home/index.html")
+    return render(request, "home/home.html")
 
 def claim(request):
-    return render(request, "claim/index.html")
+    return render(request, "claim/claim.html")
 
 def verify(request, code):
     return render(request, "verify/verify.html", {
