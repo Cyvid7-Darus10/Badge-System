a75d37f360506db9628dc6f1f9b0955982fa4c03
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2021-09-03 15:06:25 +0800

    added the claim and verify page

--- alpha/badge/views.py
+++ alpha/badge/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello!")
+
+def claim(request):
+    return HttpResponse("Claim!")
+
+def verify(request):
+    return HttpResponse("Verify!")
