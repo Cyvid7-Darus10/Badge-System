a7a08aabef01519114b619c292643f55dc5266df
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2021-09-04 21:53:23 +0800

    factorized inputs

--- alpha/badge/views.py
+++ alpha/badge/views.py
@@ -1,11 +1,21 @@
-from django.http import HttpResponse
+from django import forms
 from django.shortcuts import render
+
+class claimBadge(forms.Form):
+    code  = forms.CharField(label="Code")
+    name  = forms.CharField(label="Name")
+    email = forms.CharField(label="Email")
+
+class verifyBadge(forms.Form):
+    serial  = forms.CharField(label="Serial Code")
 
 def index(request):
     return render(request, "badge/home/home.html")
 
 def claim(request):
-    return render(request, "badge/claim/claim.html")
+    return render(request, "badge/claim/claim.html", {
+        "form": claimBadge()
+    })
 
 def view_badge(request, code):
     return render(request, "badge/verify/view.html", {
@@ -13,4 +23,6 @@ def view_badge(request, code):
     })
 
 def verify(request):
-    return render(request, "badge/verify/verify.html")
+    return render(request, "badge/verify/verify.html", {
+        "form" : verifyBadge() 
+    })
