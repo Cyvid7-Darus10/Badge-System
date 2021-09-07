a20934e874e38e15f59c09ff64e1f50a7bd44f3d
Arlan Vincent John German <arlangerman.senti@gmail.com>
2021-09-07 22:53:49 +0800

    Fixed repo, updated models, migrated db

--- alpha/badge/models.py
+++ alpha/badge/models.py
@@ -1,39 +1,53 @@
 from django.db import models
-from datetime import datetime    
+from datetime import datetime
 
-class Badge(models.Model):
+from django.db.models.fields import DateTimeField
+
+
+class Base(models.Model):
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+    deleted = DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Badge(Base):
     label = models.CharField(max_length=250)
     points = models.IntegerField()
     url = models.CharField(max_length=250)
     date = models.DateField()
-    added_on = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return f"{self.label} on {self.date} with {self.points} points"
 
-class Guilder(models.Model):
+
+class Guilder(Base):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    added_on = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return f"{self.name} - {self.email}"
 
-class Claimable(models.Model):
-    badge = models.ForeignKey(Badge, on_delete=models.CASCADE, related_name="released_badge")
+
+class Claimable(Base):
+    badge = models.ForeignKey(
+        Badge, on_delete=models.CASCADE, related_name="released_badge")
     code = models.CharField(max_length=50)
     release_on = models.DateTimeField()
     expires_on = models.DateTimeField()
-    added_on = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return f"{self.badge} | {self.code} | {self.release_on} - {self.expires_on}"
 
+
 class Claimed(models.Model):
-    guilder = models.ForeignKey(Guilder, on_delete=models.CASCADE, related_name="completer")
-    badge = models.ForeignKey(Claimable, on_delete=models.CASCADE, related_name="claimed_badge")
+    guilder = models.ForeignKey(
+        Guilder, on_delete=models.CASCADE, related_name="completer")
+    badge = models.ForeignKey(
+        Claimable, on_delete=models.CASCADE, related_name="claimed_badge")
     serial = models.CharField(max_length=50)
-    added_on = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return f"{self.guilder} | {self.badge} | {self.serial}"
