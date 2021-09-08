8525875aaac96a556a181036c3a7c02a8b6dc04e
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2021-09-09 02:57:19 +0800

    Separated the important details for security

--- alpha/alpha/settings.py
+++ alpha/alpha/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^w1buzmoo@l_dxmz&$3&t78n5svmdubc-8%j=elo2r_q=t!u$l'
-ENCRYPT_KEY = b'n0Zk2GbClW5pwPfkN7Dzo0L8nytkD5U4KEYZ3Sel5iM='
+SECRET_KEY = config('SECRET_KEY')
+ENCRYPT_KEY = config('ENCRYPT_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
